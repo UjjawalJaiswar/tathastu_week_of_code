e3c2a108614ee46ad85a0e123d0898c669919197
UjjawalJaiswar <64912904+UjjawalJaiswar@users.noreply.github.com>
2020-05-20 16:52:49 +0530

    Create Program 3.py

---
+++ day1/Program 3.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number,a= "))
+b=int(input ("Enter the second number,b="))
+a=a+b
+b=a-b
+a=a-b
+print("After swapping")
+print("Value of a=",a)
+print ("Value of b=",b)
